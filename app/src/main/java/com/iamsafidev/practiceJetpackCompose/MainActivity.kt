35420e9da37ce6543bd2c4238a41445af24762e7
imsafidev <safi.rehman@confiz.com>
2023-11-21 00:13:58 +0500

    Create a click counter functionality in compose.

--- app/src/main/java/com/iamsafidev/practiceJetpackCompose/MainActivity.kt
+++ app/src/main/java/com/iamsafidev/practiceJetpackCompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.iamsafidev.practiceJetpackCompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.iamsafidev.practiceJetpackCompose.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val counter = remember { mutableStateOf(0) }
+                    ClickCounter(counter.value) {
+                        counter.value += 1
+                    }
                 }
             }
         }
@@ -30,17 +37,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun ClickCounter(count: Int, onClick: () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = onClick) {
+            Text(text = "Counter Value: $count")
+        }
     }
 }
